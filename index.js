6e957d6dd90a0282a697426d670f387e93299d55
Husain <husaincementwalas@gmail.com>
2025-01-10 12:12:27 +0530

    ony css left

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let score = JSON.parse(localStorage.getItem('score')) || { wins:0, losses:0, ties:0};
 
+/*
+
+SHORTCUT KEYS
+1. ROCK = r or R
+2. PAPER = p or P
+3. SCISSORS = s or S
+4. AUTO PLAY = a or A
+5. RESET SCORE = 0 (zero)
 
 
 
-
+*/
 
 
 updateScoreElement();
@@ -26,13 +34,36 @@ ties:0
 document.querySelector('.js-autoplay-button').addEventListener('click',
   ()=>{
     autoPlay();
+      
   }
 );
+
+document.body.addEventListener('keydown',(event)=>{
+if(event.key === 'a' || event.key === 'A'){
+  autoPlay();
+}
+})
+
+/*document.querySelector('.js-autoplay-button').addEventListener('click',()=>{
+
+  const autoPlayButton = document.querySelector('.js-autoplay-button');
+
+  if(autoPlayButton.innerText === 'AUTO PLAY'){
+
+    autoPlayButton.innerHTML = 'STOP PLAY';
+  }
+  else{
+    autoPlayButton.innerHTML = 'AUTO PLAY';
+  }
+});*/ /* this is one way to change button on autoplay , other is inside the autoplay function*/
+
+
 
 let isAutoPlaying = false;
 let intervalId;
 
 function autoPlay(){
+
 if(!isAutoPlaying){
   
 intervalId = setInterval(() =>{ //save the intervalId in a variable
@@ -40,10 +71,19 @@ intervalId = setInterval(() =>{ //save the intervalId in a variable
   playGame(playerMove);
   },1000);
 isAutoPlaying = true;
+
+document.querySelector('.js-autoplay-button')
+.innerHTML = 'STOP PLAY';
 }
+
+
 else{
 clearInterval(intervalId); //call on the saved Id to stop autoplay
 isAutoPlaying = false;
+
+
+document.querySelector('.js-autoplay-button')
+.innerHTML = 'AUTO PLAY';
 }
 }
 
@@ -60,6 +100,11 @@ else if(event.key === 'p' || event.key === 'P'){
 }
 else if(event.key === 's' || event.key === 'S'){
   playGame('scissors');
+}
+
+else if(event.key === 'Backspace'){
+
+  showResetConfirmmation();
 }
 });
 
@@ -178,12 +223,55 @@ updateScoreElement();
 
 //here also using eventListener instead of onclick
 document.querySelector('.js-reset-button').addEventListener('click',()=>{
-  score.wins = 0;
+  
+  
+  /*score.wins = 0;
+  score.losses =0;
+  score.ties =0;
+  localStorage.removeItem('score');
+  updateScoreElement();*/
+
+  showResetConfirmmation();
+});
+
+
+function showResetConfirmmation(){
+
+  document.querySelector('.js-confirmation')
+  .innerHTML = 
+  `Are you sure you want to reset the score?
+  <button class="js-reset-confirm-yes reset-confirm-button">
+  YES
+  </button>
+  
+  <button class="js-reset-confirm-no reset-confirm-button">
+  NO
+  </button>`,
+
+  document.querySelector('.js-reset-confirm-yes')
+  .addEventListener('click',()=>{
+    score.wins = 0;
   score.losses =0;
   score.ties =0;
   localStorage.removeItem('score');
   updateScoreElement();
-});
+
+  hideResetConfirmation();
+  });
+
+  document.querySelector('.js-reset-confirm-no')
+  .addEventListener('click',()=>{
+
+    hideResetConfirmation();
+  })
+}
+
+function hideResetConfirmation(){
+
+  document.querySelector('.js-confirmation')
+  .innerHTML = '';
+}
+
 
 
 function updateScoreElement(){
